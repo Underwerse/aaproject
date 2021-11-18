7478ae1d6dc015ae0a1c4cbdecbcbab165407e3e
Underwerse <pvchernoff@gmail.com>
2021-11-18 14:03:19 +0200

    do pedometer

--- app/src/main/java/fi/aa/aaproject/MainActivity.java
+++ app/src/main/java/fi/aa/aaproject/MainActivity.java
@@ -3,12 +3,74 @@ package fi.aa.aaproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener
+{
+    SensorManager sensorManager;
+    TextView tv_Steps;
+
+    boolean isRunning = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        tv_Steps = (TextView) findViewById(R.id.textView_Steps);
+
+        tv_Steps.setText(0);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        isRunning = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null)
+        {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+        else
+        {
+            Toast.makeText(this, "STEP_COUNTER sensor not found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        isRunning = false;
+//    sensorManager.unregisterListener(this); // this will stop detecting steps!
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        if (isRunning)
+        {
+            tv_Steps.setText(String.valueOf(event.values[0]));
+            Log.i("steps", "step run");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+        // Needed this method to skip the error
+        // error: MainActivity is not abstract and does not override abstract method onAccuracyChanged(Sensor,int) in SensorEventListener
+        // public class MainActivity extends AppCompatActivity implements SensorEventListener
     }
 }
