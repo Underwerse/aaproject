86b6d8218c8ec3fc53cd4c3057ae52697b52f7b2
Underwerse <pvchernoff@gmail.com>
2021-11-21 12:25:58 +0200

    feat: add icon; refactor: change step counter code; feat: add saving current date steps count into sharedPreferences.

--- app/src/main/java/fi/aa/aaproject/MainActivity.java
+++ app/src/main/java/fi/aa/aaproject/MainActivity.java
@@ -2,8 +2,8 @@ package fi.aa.aaproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -13,12 +13,84 @@ import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/*
+public class MainActivity extends AppCompatActivity implements SensorEventListener
+{
+    private SensorManager sensorManager;
+    private TextView tv_Steps;
+    private Sensor mStepCounter;
+    private boolean isRunning = false;
+    private int stepCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        tv_Steps = (TextView) findViewById(R.id.textView_Steps);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            Toast.makeText(this, "Permission is not granted", Toast.LENGTH_SHORT).show();
+        }
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+            mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            Log.i("steps", "step");
+            isRunning = true;
+        } else {
+            Toast.makeText(this, "STEP_COUNTER sensor not found", Toast.LENGTH_SHORT).show();
+            isRunning = false;
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+            sensorManager.registerListener(this, mStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+            sensorManager.unregisterListener(this, mStepCounter);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent)
+    {
+        if (sensorEvent.sensor == mStepCounter) {
+            stepCount = (int) sensorEvent.values[0];
+            tv_Steps.setText(String.valueOf(stepCount));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}*/
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener
 {
     SensorManager sensorManager;
     TextView tv_Steps;
-
-    boolean isRunning = false;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,47 +101,44 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         tv_Steps = (TextView) findViewById(R.id.textView_Steps);
 
-    }
+        String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
 
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-        isRunning = true;
+        sharedPreferences = this.getSharedPreferences("fi.aa.aaproject", Context.MODE_PRIVATE);
+        SharedPreferences.Editor ed;
+
         Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        if (countSensor != null)
-        {
+
+        if (countSensor != null) {
             sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Sensor not found", Toast.LENGTH_SHORT).show();
         }
-        else
-        {
-            Toast.makeText(this, "STEP_COUNTER sensor not found", Toast.LENGTH_SHORT).show();
+
+        if (!sharedPreferences.contains(currentDate)) {
+            sharedPreferences.edit().putInt(currentDate, 0).apply();
         }
-    }
 
-    @Override
-    protected void onPause()
-    {
-        super.onPause();
-        isRunning = false;
-//    sensorManager.unregisterListener(this); // this will stop detecting steps!
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event)
-    {
-        if (isRunning)
-        {
-            tv_Steps.setText(String.valueOf(event.values[0]));
-            Log.i("steps", "step run");
+        if (sharedPreferences.contains(currentDate)) {
+            int v = sharedPreferences.getInt(currentDate, 1);
+            tv_Steps.setText(String.valueOf(v));
         }
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy)
-    {
-        // Needed this method to skip the error
-        // error: MainActivity is not abstract and does not override abstract method onAccuracyChanged(Sensor,int) in SensorEventListener
-        // public class MainActivity extends AppCompatActivity implements SensorEventListener
+    public void onSensorChanged(SensorEvent event) {
+        String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        Log.i("date", currentDate);
+        if (sharedPreferences.contains(currentDate)) {
+            int value = sharedPreferences.getInt(currentDate, 1);
+            value = value + 1;
+            sharedPreferences.edit().putInt(currentDate, value).apply();
+            Toast.makeText(this, String.valueOf(value), Toast.LENGTH_SHORT).show();
+            tv_Steps.setText(String.valueOf(value));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
