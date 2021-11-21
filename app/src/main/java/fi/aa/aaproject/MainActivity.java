362505abf667a56e70ad47dccc575d8054ab5a6d
Underwerse <pvchernoff@gmail.com>
2021-11-21 13:27:25 +0200

    feat: add permission request; fix: fix step counter on android 11 devices; refactor: change Main_activity LinearLayout into ConstraintLayout.

--- app/src/main/java/fi/aa/aaproject/MainActivity.java
+++ app/src/main/java/fi/aa/aaproject/MainActivity.java
@@ -1,9 +1,17 @@
 package fi.aa.aaproject;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -91,6 +99,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     SensorManager sensorManager;
     TextView tv_Steps;
     SharedPreferences sharedPreferences;
+    int ACTIVITY_RECOGNITION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -122,6 +131,47 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             int v = sharedPreferences.getInt(currentDate, 1);
             tv_Steps.setText(String.valueOf(v));
         }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(MainActivity.this, "You have already granted this permission", Toast.LENGTH_SHORT).show();
+        } else {
+            requestActivityPermission();
+        }
+    }
+
+    private void requestActivityPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACTIVITY_RECOGNITION)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission denied")
+                    .setMessage("This permission is needed for recording your activity")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACTIVITY_RECOGNITION}, ACTIVITY_RECOGNITION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACTIVITY_RECOGNITION}, ACTIVITY_RECOGNITION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == ACTIVITY_RECOGNITION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
@@ -132,7 +182,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             int value = sharedPreferences.getInt(currentDate, 1);
             value = value + 1;
             sharedPreferences.edit().putInt(currentDate, value).apply();
-            Toast.makeText(this, String.valueOf(value), Toast.LENGTH_SHORT).show();
             tv_Steps.setText(String.valueOf(value));
         }
     }
