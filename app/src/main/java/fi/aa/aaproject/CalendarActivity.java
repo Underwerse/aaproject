5a122bd1b01348ef797373d9934312d93041e654
Underwerse <pvchernoff@gmail.com>
2021-11-30 14:30:25 +0200

    feature: add three progress bars to calendar activity

--- app/src/main/java/fi/aa/aaproject/CalendarActivity.java
+++ app/src/main/java/fi/aa/aaproject/CalendarActivity.java
@@ -63,5 +63,7 @@ public class CalendarActivity extends AppCompatActivity {
                 String.valueOf(dataProcessor.getInt(date +
                         ",sleep"));
         tvCalDreams.setText(updatedDreams);
+
+
     }
 }
