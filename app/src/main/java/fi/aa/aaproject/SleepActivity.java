32ff3af019d5d0ee3f5edb24d42c3a58107597dd
dimitri <kouluduunii@gmail.com>
2021-12-15 12:47:18 +0200

    uni ja vedenseuranta

--- app/src/main/java/fi/aa/aaproject/SleepActivity.java
+++ app/src/main/java/fi/aa/aaproject/SleepActivity.java
@@ -1,24 +1,94 @@
 package fi.aa.aaproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class SleepActivity extends AppCompatActivity {
     private ProgressBar pbSleep;
+    private Button nappi;
+    private Button nappi2;
+    private int progress = 0;
+    private TextView teksti1;
+    private int unenmaara = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sleep);
+        DataProcessor dataProsessori = new DataProcessor(this  );
+        String currentDate = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(new Date());
+        updateProgress();
 
         Animation animation = (Animation) AnimationUtils.loadAnimation(this, R.anim.pb_animation);
 
         pbSleep = findViewById(R.id.pb_sleep);
-        pbSleep.setProgress(70);
+        pbSleep.setProgress(0);
         pbSleep.startAnimation(animation);
+
+        nappi = (Button) findViewById(R.id.nappi);
+        nappi2 = (Button) findViewById(R.id.nappi2);
+        teksti1 = (TextView) findViewById(R.id.unenMaara);
+
+        nappi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (progress <=90){
+                    progress += 10;
+                    unenmaara += 1;
+                    teksti1.setText(unenmaara +" H");
+                    dataProsessori.setInt(currentDate +",sleep",unenmaara);
+                    updateProgress();
+                }
+            }
+        });
+
+        nappi2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (progress >= 10){
+                    progress -= 10;
+                    unenmaara -= 1;
+                    teksti1.setText(unenmaara +" H");
+                    dataProsessori.setInt(currentDate +",sleep",unenmaara);
+                    updateProgress();
+                }
+            }
+        });
+
     }
+    private void updateProgress(){
+        ProgressBar uni = (ProgressBar) findViewById(R.id.pb_sleep);
+
+        uni.setProgress(progress);
+
+    }
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String stateSaved = savedInstanceState.getString("saved_state:");
+
+
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("saved_state:", "aaa");
+
+    }
+
 }
--- app/src/main/java/fi/aa/aaproject/WaterActivity.java
+++ app/src/main/java/fi/aa/aaproject/WaterActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,17 +18,16 @@ import java.util.Locale;
 
 public class WaterActivity extends AppCompatActivity {
 
-    private Button nappiNayta;
-    private Button nappiResetoi;
-    private ImageView getKuva0;
-    private ImageView getKuva1;
-    private ImageView getKuva2;
-    private ImageView getKuva3;
-    private ImageView getKuva4;
-    private ImageView getKuva5;
-    private ImageView getKuva6;
-    private ImageView getKuva7;
-    private ImageView getKuva8;
+    private Button nappi;
+    private Button nappi2;
+    private TextView teksti1;
+    private int progress = 0;
+    private int vesimaara = 0;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,145 +35,51 @@ public class WaterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_water);
         DataProcessor dataProsessori = new DataProcessor(this  );
         String currentDate = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(new Date());
+        updateProgress();
 
-         Button nappiNayta;
-         ImageView getKuva0;
-         ImageView getKuva1;
-         ImageView getKuva2;
-         ImageView getKuva3;
-         ImageView getKuva4;
-         ImageView getKuva5;
-         ImageView getKuva6;
-         ImageView getKuva7;
-         ImageView getKuva8;
+        nappi = (Button) findViewById(R.id.nappi);
+        nappi2 = (Button) findViewById(R.id.nappi2);
+        teksti1 = (TextView) findViewById(R.id.counterView);
 
 
-         TextView textML1;
-         TextView textML2;
-         TextView textML3;
-         TextView textML4;
-         TextView textML5;
-         TextView textML6;
-         TextView textML7;
-         TextView textML8;
-         TextView textML9;
-
-        getKuva0 = (ImageView) findViewById(R.id.kuva0);
-        getKuva1 = (ImageView) findViewById(R.id.kuva1);
-        getKuva2 = (ImageView) findViewById(R.id.kuva2);
-        getKuva3 = (ImageView) findViewById(R.id.kuva3);
-        getKuva4 = (ImageView) findViewById(R.id.kuva4);
-        getKuva5 = (ImageView) findViewById(R.id.kuva5);
-        getKuva6 = (ImageView) findViewById(R.id.kuva6);
-        getKuva7 = (ImageView) findViewById(R.id.kuva7);
-        getKuva8 = (ImageView) findViewById(R.id.kuva8);
-
-
-
-        textML1 = (TextView) findViewById(R.id.textView);
-        textML2 = (TextView) findViewById(R.id.textView2);
-        textML3 = (TextView) findViewById(R.id.textView3);
-        textML4 = (TextView) findViewById(R.id.textView5);
-        textML5 = (TextView) findViewById(R.id.textView6);
-        textML6 = (TextView) findViewById(R.id.textView7);
-        textML7 = (TextView) findViewById(R.id.textView9);
-        textML8 = (TextView) findViewById(R.id.textView10);
-        textML9 = (TextView) findViewById(R.id.textView11);
-
-
-
-        nappiNayta = (Button) findViewById(R.id.btnPlus);
-
-
-        nappiNayta.setOnClickListener(new View.OnClickListener() {
+        nappi.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v1) {
-                getKuva0.setVisibility(View.INVISIBLE);
-                textML1.setVisibility(View.VISIBLE);
-                dataProsessori.setInt(currentDate + ",water",250);
-
-                nappiNayta.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        getKuva1.setVisibility(View.INVISIBLE);
-                        textML1.setVisibility(View.INVISIBLE);
-                        textML2.setVisibility(View.VISIBLE);
-                        dataProsessori.setInt(currentDate + ",water",500);
-
-                        nappiNayta.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                getKuva2.setVisibility(View.INVISIBLE);
-                                textML2.setVisibility(View.INVISIBLE);
-                                textML3.setVisibility(View.VISIBLE);
-                                dataProsessori.setInt(currentDate + ",water",750);
-                                Log.i("jotain", String.valueOf(dataProsessori.getInt(currentDate + ",water")));
-
-                                nappiNayta.setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        getKuva3.setVisibility(View.INVISIBLE);
-                                        textML3.setVisibility(View.INVISIBLE);
-                                        textML4.setVisibility(View.VISIBLE);
-                                        dataProsessori.setInt(currentDate + ",water",1000);
-                                        nappiNayta.setOnClickListener(new View.OnClickListener() {
-                                            @Override
-                                            public void onClick(View v) {
-                                                getKuva4.setVisibility(View.INVISIBLE);
-                                                textML4.setVisibility(View.INVISIBLE);
-                                                textML5.setVisibility(View.VISIBLE);
-                                                dataProsessori.setInt(currentDate + ",water",1250);
-                                                nappiNayta.setOnClickListener(new View.OnClickListener() {
-                                                    @Override
-                                                    public void onClick(View v) {
-                                                        getKuva5.setVisibility(View.INVISIBLE);
-                                                        textML5.setVisibility(View.INVISIBLE);
-                                                        textML6.setVisibility(View.VISIBLE);
-                                                        dataProsessori.setInt(currentDate + ",water",1500);
-                                                        nappiNayta.setOnClickListener(new View.OnClickListener() {
-                                                            @Override
-                                                            public void onClick(View v) {
-                                                                getKuva6.setVisibility(View.INVISIBLE);
-                                                                textML6.setVisibility(View.INVISIBLE);
-                                                                textML7.setVisibility(View.VISIBLE);
-                                                                dataProsessori.setInt(currentDate + ",water",1750);
-                                                                nappiNayta.setOnClickListener(new View.OnClickListener() {
-                                                                    @Override
-                                                                    public void onClick(View v) {
-                                                                        getKuva7.setVisibility(View.INVISIBLE);
-                                                                        textML7.setVisibility(View.INVISIBLE);
-                                                                        textML8.setVisibility(View.VISIBLE);
-                                                                        dataProsessori.setInt(currentDate + ",water",2000);
-                                                                        nappiNayta.setOnClickListener(new View.OnClickListener() {
-                                                                            @Override
-                                                                            public void onClick(View v) {
-                                                                                getKuva8.setVisibility(View.INVISIBLE);
-                                                                                textML8.setVisibility(View.INVISIBLE);
-                                                                                textML9.setVisibility(View.VISIBLE);
-                                                                                dataProsessori.setInt(currentDate + ",water",2250);
-
-                                                                            }
-                                                                        });
-                                                                    }
-                                                                });
-                                                            }
-                                                        });
-                                                    }
-                                                });
-                                            }
-                                        });
-
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-
+            public void onClick(View v) {
+                if (progress <=90){
+                    progress += 10;
+                    vesimaara += 250;
+                    teksti1.setText(vesimaara +" ML");
+                    dataProsessori.setInt(currentDate +",water",vesimaara);
+                    updateProgress();
+                }
             }
         });
+        nappi2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (progress >= 10){
+                    progress -= 10;
+                    vesimaara -= 250;
+                    teksti1.setText(vesimaara +" ML");
+                    dataProsessori.setInt(currentDate +",water",vesimaara);
+                    updateProgress();
+
+                }
+            }
+        });
+
     }
+
+    private void updateProgress(){
+        ProgressBar lasi = (ProgressBar) findViewById(R.id.progressBar);
+
+        lasi.setProgress(progress);
+
+    }
+
+
+
+
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
